d6a869214245c6ff4f9c4c6510592b8a9f64504e
linx-7 <xuyaoalice@gmail.com>
2024-10-04 14:30:35 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 
 
 int main(int argc, char* argv[]){
-    std::list<std::string>::iterator itr;
-    
+    intList.push_back(temp);
+    }
+
+    std::list<int>::iterator it = intList.begin();
+    while (it != intList.end()) {
+        std::cout << *it << std::endl;
+    std::list<int>::reverse_iterator it = intList.rbegin();
+    while (it != intList.rend()) {
+        std::cout << (*it)*(*it) << std::endl;
+        it++;
 }
