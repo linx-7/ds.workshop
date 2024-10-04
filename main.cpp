ba48526827dd8e1dbdef95a7d0662bce40df2c40
linx-7 <xuyaoalice@gmail.com>
2024-10-04 14:46:29 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
-#include <string>
+#include <list>
+#include <iterator> 
+#include <cstdlib>  
 
+int main(int argc, char* argv[]) {
 
-int main(int argc, char* argv[]){
-    intList.push_back(temp);
+    std::list<int> intList;
+
+    for (int i = 1; i < argc; ++i) {
+        int temp = std::atoi(argv[i]);
+        intList.push_back(temp);
     }
 
     std::list<int>::iterator it = intList.begin();
     while (it != intList.end()) {
         std::cout << *it << std::endl;
-    std::list<int>::reverse_iterator it = intList.rbegin();
-    while (it != intList.rend()) {
-        std::cout << (*it)*(*it) << std::endl;
         it++;
+    }
+
+    return 0;
 }
